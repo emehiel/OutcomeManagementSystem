d2daf721b44e2545e974988e8735a6d22c21aee3
Eric Mehiel <emehiel@calpoly.edu>
2021-08-23 12:02:04 -0700

    Clean everything up and then port some of the code to new project with user account support.

--- Migrations/20210823190013_initialCreate.cs
+++ Migrations/20210823190013_initialCreate.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace OutcomeManagementSystem.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class initialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "CourseCoordinators",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseCoordinators", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProgramOutcome",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false),
+                    PONumber = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Concentration = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgramOutcome", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentOutcome",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false),
+                    SONumber = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    ShortName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentOutcome", x => x.ID);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Course",
                 columns: table => new
@@ -19,86 +62,17 @@ namespace OutcomeManagementSystem.Migrations
                     Description = table.Column<string>(nullable: true),
                     Concentration = table.Column<string>(nullable: true),
                     Year = table.Column<int>(nullable: false),
-                    Quarter = table.Column<int>(nullable: false)
+                    Quarter = table.Column<int>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    CourseCoordinatorID = table.Column<int>(nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Course", x => x.ID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "ProgramOutcome",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false),
-                    Description = table.Column<string>(nullable: true),
-                    ShortName = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ProgramOutcome", x => x.ID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "StudentOutcome",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false),
-                    Description = table.Column<string>(nullable: true),
-                    ShortName = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_StudentOutcome", x => x.ID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "CourseCoordinators",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FirstName = table.Column<string>(nullable: true),
-                    LastName = table.Column<string>(nullable: true),
-                    CourseID = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_CourseCoordinators", x => x.ID);
                     table.ForeignKey(
-                        name: "FK_CourseCoordinators_Course_CourseID",
-                        column: x => x.CourseID,
-                        principalTable: "Course",
-                        principalColumn: "ID",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "PreReqMaps",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    PreReqDept = table.Column<string>(nullable: true),
-                    PreReqNumber = table.Column<int>(nullable: false),
-                    CourseDept = table.Column<string>(nullable: true),
-                    CourseNumber = table.Column<int>(nullable: false),
-                    PreReqID = table.Column<int>(nullable: false),
-                    CourseID = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PreReqMaps", x => x.ID);
-                    table.ForeignKey(
-                        name: "FK_PreReqMaps_Course_CourseID",
-                        column: x => x.CourseID,
-                        principalTable: "Course",
-                        principalColumn: "ID",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_PreReqMaps_Course_PreReqID",
-                        column: x => x.PreReqID,
-                        principalTable: "Course",
+                        name: "FK_Course_CourseCoordinators_CourseCoordinatorID",
+                        column: x => x.CourseCoordinatorID,
+                        principalTable: "CourseCoordinators",
                         principalColumn: "ID",
                         onDelete: ReferentialAction.Cascade);
                 });
@@ -109,7 +83,8 @@ namespace OutcomeManagementSystem.Migrations
                 {
                     ID = table.Column<int>(nullable: false),
                     Description = table.Column<string>(nullable: true),
-                    StudentOutcomeID = table.Column<int>(nullable: false)
+                    StudentOutcomeID = table.Column<int>(nullable: false),
+                    KPINumber = table.Column<int>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -123,6 +98,65 @@ namespace OutcomeManagementSystem.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Assessments",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CanvasAssessmentID = table.Column<int>(nullable: false),
+                    SubmissionScore = table.Column<double>(nullable: false),
+                    SubmissionDate = table.Column<DateTime>(nullable: false),
+                    OutcomeName = table.Column<string>(nullable: true),
+                    CanvasOutcomeID = table.Column<int>(nullable: false),
+                    OutcomeScore = table.Column<double>(nullable: false),
+                    CanvasCourseID = table.Column<int>(nullable: false),
+                    OutcomePointsPossible = table.Column<double>(nullable: false),
+                    OutcomeMasteryScore = table.Column<double>(nullable: false),
+                    CourseID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assessments", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Assessments_Course_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Course",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PreReqs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(nullable: false),
+                    CourseDept = table.Column<string>(nullable: true),
+                    CourseNumber = table.Column<int>(nullable: false),
+                    PreReqDept = table.Column<string>(nullable: true),
+                    PreReqNumber = table.Column<int>(nullable: false),
+                    CourseID = table.Column<int>(nullable: false),
+                    ReqCourseID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PreReqs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PreReqs_Course_CourseID",
+                        column: x => x.CourseID,
+                        principalTable: "Course",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PreReqs_Course_ReqCourseID",
+                        column: x => x.ReqCourseID,
+                        principalTable: "Course",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "CLO",
                 columns: table => new
                 {
@@ -130,7 +164,8 @@ namespace OutcomeManagementSystem.Migrations
                     CourseID = table.Column<int>(nullable: false),
                     Description = table.Column<string>(nullable: true),
                     SO_KPIID = table.Column<int>(nullable: false),
-                    ProgramOutcomeID = table.Column<int>(nullable: false)
+                    ProgramOutcomeID = table.Column<int>(nullable: false),
+                    MasteryLevel = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
@@ -156,6 +191,11 @@ namespace OutcomeManagementSystem.Migrations
                 });
 
             migrationBuilder.CreateIndex(
+                name: "IX_Assessments_CourseID",
+                table: "Assessments",
+                column: "CourseID");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_CLO_CourseID",
                 table: "CLO",
                 column: "CourseID");
@@ -171,20 +211,19 @@ namespace OutcomeManagementSystem.Migrations
                 column: "SO_KPIID");
 
             migrationBuilder.CreateIndex(
-                name: "IX_CourseCoordinators_CourseID",
-                table: "CourseCoordinators",
-                column: "CourseID",
-                unique: true);
+                name: "IX_Course_CourseCoordinatorID",
+                table: "Course",
+                column: "CourseCoordinatorID");
 
             migrationBuilder.CreateIndex(
-                name: "IX_PreReqMaps_CourseID",
-                table: "PreReqMaps",
+                name: "IX_PreReqs_CourseID",
+                table: "PreReqs",
                 column: "CourseID");
 
             migrationBuilder.CreateIndex(
-                name: "IX_PreReqMaps_PreReqID",
-                table: "PreReqMaps",
-                column: "PreReqID");
+                name: "IX_PreReqs_ReqCourseID",
+                table: "PreReqs",
+                column: "ReqCourseID");
 
             migrationBuilder.CreateIndex(
                 name: "IX_SO_KPI_StudentOutcomeID",
@@ -195,13 +234,13 @@ namespace OutcomeManagementSystem.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "Assessments");
+
+            migrationBuilder.DropTable(
                 name: "CLO");
 
             migrationBuilder.DropTable(
-                name: "CourseCoordinators");
-
-            migrationBuilder.DropTable(
-                name: "PreReqMaps");
+                name: "PreReqs");
 
             migrationBuilder.DropTable(
                 name: "ProgramOutcome");
@@ -214,6 +253,9 @@ namespace OutcomeManagementSystem.Migrations
 
             migrationBuilder.DropTable(
                 name: "StudentOutcome");
+
+            migrationBuilder.DropTable(
+                name: "CourseCoordinators");
         }
     }
 }
